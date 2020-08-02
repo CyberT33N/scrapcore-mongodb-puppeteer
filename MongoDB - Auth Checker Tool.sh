71a4e1d8214d85c75e79886a5b662f4f783b4137
ᑕƳᗷᗴᖇ丅33ᑎ™  ㋛⎠ <17134633+CyberT33N@users.noreply.github.com>
2020-08-02 23:54:55 +0200

    Delete MongoDB - Auth Checker Tool.sh

--- MongoDB - Auth Checker Tool.sh
+++
@@ -1,9 +0,0 @@
-#pwd <-- current directory
-#-------------------------------------------------------------------------------
-cd "$(dirname "$0")"
-pwd
-printf "\nWe will display now the current directory used:"
-echo "$(dirname "$0")"
-printf "\n\nWe will check now your MongoDB Connection..\n\nPlease wait.. This maybe take some time..\n"
-sudo "MongoDB - Auth Checker Tool.js"
-printf "\nWe finished the .sh file :) - Created by Dennis Demand( github.com/CyberT33N )\n"
