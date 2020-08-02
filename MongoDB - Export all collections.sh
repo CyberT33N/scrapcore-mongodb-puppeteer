524ac5309e713f59157f0a2f56651e56483f37bf
ᑕƳᗷᗴᖇ丅33ᑎ™  ㋛⎠ <17134633+CyberT33N@users.noreply.github.com>
2020-08-02 23:55:07 +0200

    Delete MongoDB - Export all collections.sh

--- MongoDB - Export all collections.sh
+++
@@ -1,9 +0,0 @@
-#pwd <-- current directory
-#-------------------------------------------------------------------------------
-cd "$(dirname "$0")"
-pwd
-printf "\nWe will display now the current directory used:"
-echo "$(dirname "$0")"
-printf "\n\nWe will start now: MongoDB - Export all collections.js..\n\nPlease wait.. This maybe take some time..\n"
-node "MongoDB - Export all collections.js"
-printf "\nWe finished the .sh file :) - Created by Dennis Demand( github.com/CyberT33N )\n"
