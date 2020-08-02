ec418de2e732832e82e02d3edb136f10c667c390
ᑕƳᗷᗴᖇ丅33ᑎ™  ㋛⎠ <17134633+CyberT33N@users.noreply.github.com>
2020-08-02 23:52:14 +0200

    Add files via upload

---
+++ INSTALL THIS/SOFTWARE/Node.js/Linux/#NODE.JS v12.x - REMOVE FIRST - THEN REINSTALL - UBUNTU&MINT.sh
@@ -0,0 +1,23 @@
+cd "$(dirname "$0")"
+pwd
+printf "\nWe will display now the current directory used:"
+echo "$(dirname "$0")"
+printf "\n\nWe will deinstall now everything with Node.js..\n"
+#-------------------------------------------------------------------------------
+sudo apt-get remove -y npm
+sudo apt-get remove -y nodejs-legacy
+sudo apt-get remove -y nodejs
+sudo rm /usr/bin/node
+sudo rm /usr/local/node
+sudo apt -y autoremove
+#-------------------------------------------------------------------------------
+printf "\n\nWe will download an install now Node.js 12.x\n"
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs
+#-------------------------------------------------------------------------------
+printf "\n\nWe will display now the current installed version of node:\n"
+node -v
+printf "\n\nWe will display now the current installed version of npm:\n"
+npm -v
+#-------------------------------------------------------------------------------
+printf "\nWe finished the .sh file :) - Created by Dennis Demand( https://github.com/CyberT33N )\n"
