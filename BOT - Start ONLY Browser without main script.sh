8b012a53467e7b184652f3c00f5a59de6028a5e6
ᑕƳᗷᗴᖇ丅33ᑎ™  ㋛⎠ <17134633+CyberT33N@users.noreply.github.com>
2020-08-02 23:54:21 +0200

    Delete BOT - Start ONLY Browser without main script.sh

--- BOT - Start ONLY Browser without main script.sh
+++
@@ -1,9 +0,0 @@
-#pwd <-- current directory
-#-------------------------------------------------------------------------------
-cd "$(dirname "$0")"
-pwd
-printf "\nWe will display now the current directory used:"
-echo "$(dirname "$0")"
-printf "\n\nWe will start now your browser for this bot! The scrap process will NOT be started! Only the browser window will start\n\nPlease wait.. This maybe take some time..\n"
-node "BOT - Start ONLY Browser without main script.js"
-printf "\nWe finished the .sh file :) - Created by Dennis Demand( github.com/CyberT33N )\n"
