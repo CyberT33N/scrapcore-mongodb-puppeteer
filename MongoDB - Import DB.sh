257d33fb68ebafb4e0bb1f9c82868180f8e62b75
ᑕƳᗷᗴᖇ丅33ᑎ™  ㋛⎠ <17134633+CyberT33N@users.noreply.github.com>
2020-08-02 23:55:18 +0200

    Delete MongoDB - Import DB.sh

--- MongoDB - Import DB.sh
+++
@@ -1,9 +0,0 @@
-#pwd <-- current directory
-#-------------------------------------------------------------------------------
-cd "$(dirname "$0")"
-pwd
-printf "\nWe will display now the current directory used:"
-echo "$(dirname "$0")"
-printf "\n\nWe will start now: MongoDB - Import DB.js..\n\nPlease wait.. This maybe take some time..\n"
-node "MongoDB - Import DB.js"
-printf "\nWe finished the .sh file :) - Created by Dennis Demand( github.com/CyberT33N )\n"
