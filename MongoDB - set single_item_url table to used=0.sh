ae5782418fb1afc4b90202f31a33bdcd10246e4e
ᑕƳᗷᗴᖇ丅33ᑎ™  ㋛⎠ <17134633+CyberT33N@users.noreply.github.com>
2020-08-02 23:56:17 +0200

    Delete MongoDB - set single_item_url table to used=0.sh

--- MongoDB - set single_item_url table to used=0.sh
+++
@@ -1,9 +0,0 @@
-#pwd <-- current directory
-#-------------------------------------------------------------------------------
-cd "$(dirname "$0")"
-pwd
-printf "\nWe will display now the current directory used:"
-echo "$(dirname "$0")"
-printf "\n\nWe will mark now your single_item_url collection as used=0\n\nPlease wait.. This maybe take some time..\n"
-node "MongoDB - set single_item_url table to used=0.js"
-printf "\nWe finished the .sh file :) - Created by Dennis Demand( github.com/CyberT33N )\n"
