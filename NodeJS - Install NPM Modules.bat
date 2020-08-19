d3028f244a9dcefc3096f235b36ca11951d6a389
ᑕƳᗷᗴᖇ丅33ᑎ™  ㋛⎠ <17134633+CyberT33N@users.noreply.github.com>
2020-08-19 21:53:41 +0200

    Update NodeJS - Install NPM Modules.bat
    add express

--- NodeJS - Install NPM Modules.bat
+++ NodeJS - Install NPM Modules.bat
@@ -1,4 +1,4 @@
 cd /d %~dp0
 color 7
-npm i unique-names-generator password-generator puppeteer puppeteer-page-proxy moment dookie node-os-utils uuid mongodb number-to-words fs-extra circular-json path mime-types readline googleapis@27 google-auth-library dateformat chalk chalk-animation gradient-string randomatic fancy-log object-reader unq request simple-timestamp fancy-log html-entities cheerio
+npm i express unique-names-generator password-generator puppeteer puppeteer-page-proxy moment dookie node-os-utils uuid mongodb number-to-words fs-extra circular-json path mime-types readline googleapis@27 google-auth-library dateformat chalk chalk-animation gradient-string randomatic fancy-log object-reader unq request simple-timestamp fancy-log html-entities cheerio
 pause
