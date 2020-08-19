e8d3c66670ec8c78665757a7a8966bd4c91b8d76
ᑕƳᗷᗴᖇ丅33ᑎ™  ㋛⎠ <17134633+CyberT33N@users.noreply.github.com>
2020-08-19 21:54:01 +0200

    Update NodeJS - Install NPM Modules.sh
    add express

--- NodeJS - Install NPM Modules.sh
+++ NodeJS - Install NPM Modules.sh
@@ -5,5 +5,5 @@ pwd
 printf "\nWe will display now the current directory used:"
 echo "$(dirname "$0")"
 printf "\nWe will start now to install your npm modules..\n\nPlease wait.. This maybe take some time..\n"
-npm i unique-names-generator password-generator puppeteer puppeteer-page-proxy moment dookie uuid node-os-utils mongodb number-to-words fs-extra circular-json path mime-types readline googleapis@27 google-auth-library dateformat chalk chalk-animation gradient-string randomatic fancy-log object-reader unq request simple-timestamp fancy-log html-entities cheerio
+npm i express unique-names-generator password-generator puppeteer puppeteer-page-proxy moment dookie uuid node-os-utils mongodb number-to-words fs-extra circular-json path mime-types readline googleapis@27 google-auth-library dateformat chalk chalk-animation gradient-string randomatic fancy-log object-reader unq request simple-timestamp fancy-log html-entities cheerio
 printf "\nWe finished the .sh file :)! - Created by Dennis Demand( github.com/CyberT33N )\n"
